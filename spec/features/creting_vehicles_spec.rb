14e88d0599397626ed149149295bc20b250d2ce6
Harley Inbody <hinbody@gmail.com>
2015-02-16 22:29:01 -0500

    get postgres setup and write first failing test'

---
+++ spec/features/creting_vehicles_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature "Creating Vehicles" do
+  scenario "A user can create a new vehicle do" do
+    visit "/"
+
+    click_link "New Vehicle"
+
+    fill_in "Name", with: "Truck"
+    fill_in "Make", with: "Chevy"
+    fill_in "Model", with: "Colorado"
+    fill_in "Year", with: "2006"
+    fill_in "Engine", with: "v6"
+    click_button "Create Vehicle"
+
+    expect(page).to have_content("Vehicle has been created.")
+  end
+end
